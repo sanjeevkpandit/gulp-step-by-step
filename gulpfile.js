a3115c3cf46806a24391b19aeb342af9e82daaf8
sanjeevkpandit <sanjeevpandit@lftechnology.com>
2016-02-23 23:52:30 +0545

    Gulp watch and notify

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,47 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var babel = require('gulp-babel');
+var notify = require('gulp-notify');
 var minify = require('gulp-minify');
 var concat = require('gulp-concat');
 
+var SCSS_FILES = 'src/sass/**/*.scss';
+var JS_FILES = 'src/js/**/*.js';
+var ES6_FILES = 'src/js/es6/**/*.js';
+
 gulp.task('sass', function() {
-    return gulp.src('src/sass/**/*.scss')
+    return gulp.src(SCSS_FILES)
         .pipe(sass())
-        .pipe(gulp.dest('public/css/'));
+        .pipe(gulp.dest('public/css/'))
+        .pipe(notify({
+            message: "SCSS completed"
+        }));
 });
 
 gulp.task('concat-css', function() {
-    return gulp.src('src/sass/**/*.scss')
+    return gulp.src(SCSS_FILES)
         .pipe(sass())
         .pipe(concat('compiled.scss'))
         .pipe(gulp.dest('public/css/'));
 });
 
 gulp.task('concat-js', function() {
-    return gulp.src('src/js/**/*.js')
+    return gulp.src(JS_FILES)
         .pipe(concat('compiled.js'))
         .pipe(gulp.dest('public/js/'));
 });
 
 gulp.task('es6', function() {
-    return gulp.src('src/js/es6/**/*.js')
+    return gulp.src(ES6_FILES)
         .pipe(babel())
-        .pipe(gulp.dest('public/js/'));
+        .pipe(gulp.dest('public/js/'))
+        .pipe(notify({
+            message: "ES6 Compiled"
+        }));
 });
 
 gulp.task('minify-js', function() {
-    return gulp.src('src/js/**/*.js')
+    return gulp.src(JS_FILES)
         .pipe(concat('compiled.js'))
         .pipe(minify())
         .pipe(gulp.dest('public/js/'));
@@ -39,4 +50,10 @@ gulp.task('minify-js', function() {
 // Default task
 gulp.task('default', function() {
 
+});
+
+// Watch files
+gulp.task('watch', function() {
+    gulp.watch(SCSS_FILES, ['sass']);
+    gulp.watch(ES6_FILES, ['es6']);
 });
