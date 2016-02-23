bc5c07655a13f063c8fe0192cf11f0467b14d12f
sanjeevkpandit <sanjeevpandit@lftechnology.com>
2016-02-23 22:56:17 +0545

    Concat css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass')
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
 gulp.task('sass', function() {
     return gulp.src('src/sass/**/*.scss')
         .pipe(sass())
+        .pipe(gulp.dest('public/css/'));
+});
+
+gulp.task('concat-css', function() {
+    return gulp.src('src/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(concat('compiled.scss'))
         .pipe(gulp.dest('public/css/'));
 });
 
